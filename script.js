c5206358aba9c3c957155cfba394e06e72ad73ef
Mwizv <ankielaw817+github@gmail.com>
2026-07-04 20:04:19 +1200

    Afghanistan working

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const countries = [{
     name: "Afghanistan",
-    code: "AF"
+    code: "AF",
+    numeric: "004",
+
+    continent: "Asia",
+    telephone_code: "+93",
+    utc: "+04:00",
+    hexcodes: "FFFFFF 000000",
+    capital: "Kabul",
+    currency: "؋ Afghan Afghani (AFN)"
 }];
 
 const grid = document.querySelector(".grid");
@@ -9,8 +17,8 @@ countries.forEach(country => {
     grid.innerHTML += `
         <div class="card">
             <img src="https://flagcdn.com/w320/${country.code.toLowerCase()}.png">
-            <h3>${country.name}</h3>
-            <p>${country.code}</p>
+            <h3>${country.name} ${country.code} ${country.numeric} </h3>
+            <p>${country.continent}, ${country.telephone_code}, UTC${country.utc}, ${country.hexcodes}, ${country.capital}, ${country.currency}</p>
         </div>
     `;
 });
